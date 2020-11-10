6de3e8ae08c2d6fdd26b7c05e1709791365c54b4
Emanuel Gjoka <p21egjoka@fairfieldprep.org>
2020-11-09 21:04:42 -0500

    HW 7-1
    Homework

---
+++ hw7-1.py
@@ -0,0 +1,33 @@
+# Author: Emanuel Mark Gjoka (AMDG) 11/9/2020
+
+#1
+
+print("Python"[2])
+
+#2
+
+print("Strings are simply a sequence of characters."[30:31])
+
+#3
+print(len("excellent"))
+
+#4
+print("apple" in "pineapple")
+
+#5
+print("peach" in "pear")
+
+#6
+print("x" * 100)
+
+#7
+print("54321")
+
+#8
+print("p" not in "pineapple")
+
+#9
+print("Livin' On a Prayer is my friend's favorite song.")
+
+#10
+print(len("Livin' On a Prayer is my friend's favorite song."))
